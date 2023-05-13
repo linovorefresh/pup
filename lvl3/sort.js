e3d8d1b19a02885ee5a9552e776012181fb4ae8f
linovorefresh <linolp@gmail.com>
2023-05-12 20:41:11 -0700

    create folders leveling

--- lvl3/sort.js
+++ lvl3/sort.js
@@ -1,4 +1,4 @@
 //import the regularSeasonStats array from the data file
-import { players, regularSeasonStats } from "./data";
+import { players, regularSeasonStats } from "../data";
 
 //return the array in descending order by points. DIFFICULTY LVL.3
